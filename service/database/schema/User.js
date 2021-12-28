0dd0d0ccffc2b155bf3e47eab186badf60fb00cb
unknown <zhangqingshan@talkweb.com.cn>
2021-12-28 08:59:04 +0800

    修复因escymock没数据的问题

--- service/database/schema/User.js
+++ service/database/schema/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 const SALT_WORK_FACTOR = 10
 
 //创建UserSchema
@@ -17,28 +17,28 @@ const userSchema = new Schema({
 //   }
 )
 //每次存储数据时都要执行
-userSchema.pre('save', function (next) {
-  //let user = this
-  console.log(this)
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) return next(err)
-    bcrypt.hash(this.password, salt, (err, hash) => {
-      if (err) return next(err)
-      this.password = hash
-      next()
-    })
-  })
-})
+// userSchema.pre('save', function (next) {
+//   //let user = this
+//   console.log(this)
+//   bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+//     if (err) return next(err)
+//     bcrypt.hash(this.password, salt, (err, hash) => {
+//       if (err) return next(err)
+//       this.password = hash
+//       next()
+//     })
+//   })
+// })
 
-userSchema.methods = {
-  comparePassword: (_password, password) => {
-    return new Promise((resolve, reject) => {
-      bcrypt.compare(_password, password, (err, isMatch) => {
-        if (!err) resolve(isMatch)
-        else reject(err)
-      })
-    })
-  }
-}
+// userSchema.methods = {
+//   comparePassword: (_password, password) => {
+//     return new Promise((resolve, reject) => {
+//       bcrypt.compare(_password, password, (err, isMatch) => {
+//         if (!err) resolve(isMatch)
+//         else reject(err)
+//       })
+//     })
+//   }
+// }
 // 发布模型
 mongoose.model("User", userSchema)
